5adb8fd9b2b2167ae847377c7847b3789f271746
KINGAKWO <kingakwomakembe@gmail.com>
2025-05-10 03:22:29 +0100

    feat: add turn-based functionality to tic-tac-toe game

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -7,11 +7,20 @@ function Square({value, onsquareClick}){
   )
 }
 export default function Board() {
+  const [xIsNext, setXisNext] = useState(true);
   const [squares,setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
+    if(squares[i]){
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if(xIsNext){
+      nextSquares[i] = 'X';
+    }else{
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXisNext(!xIsNext);
   }
   return(
     <>
