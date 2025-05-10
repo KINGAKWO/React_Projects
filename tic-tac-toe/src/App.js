e9862a6b2200d6cf9fb031a18816f849ea0f2d79
KINGAKWO <kingakwomakembe@gmail.com>
2025-05-10 03:05:47 +0100

    fix: update handleClick function to accept index parameter

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -8,27 +8,27 @@ function Square({value, onsquareClick}){
 }
 export default function Board() {
   const [squares,setSquares] = useState(Array(9).fill(null));
-  function handleClick() {
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = 'X';
+    nextSquares[i] = 'X';
     setSquares(nextSquares);
   }
   return(
     <>
     <div className="board-row">
-     <Square value={squares[0]} onsquareClick={handleClick}/>
-     <Square value={squares[1]}/>
-     <Square value={squares[2]}/>
+     <Square value={squares[0]} onsquareClick={()=> handleClick(0)}/>
+     <Square value={squares[1]} onsquareClick={()=> handleClick(1)}/>
+     <Square value={squares[2]} onsquareClick={()=> handleClick(2)}/>
     </div>
     <div className="board-row">
-    <Square value={squares[3]}/>
-    <Square value={squares[4]}/>
-    <Square value={squares[5]}/>
+    <Square value={squares[3]} onsquareClick={()=> handleClick(3)}/>
+    <Square value={squares[4]} onsquareClick={()=> handleClick(4)}/>
+    <Square value={squares[5]} onsquareClick={()=> handleClick(5)}/>
     </div>
     <div className="board-row">
-    <Square value={squares[6]}/>
-    <Square value={squares[7]}/>
-    <Square value={squares[8]}/>
+    <Square value={squares[6]} onsquareClick={()=> handleClick(6)}/>
+    <Square value={squares[7]} onsquareClick={()=> handleClick(7)}/>
+    <Square value={squares[8]} onsquareClick={()=> handleClick(8)}/>
     </div>
     </>
   ) 
